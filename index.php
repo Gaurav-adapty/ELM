640be719a1b823123659d0d0180fdde68510bc6d
Gaurav-adapty <gaaurav.kotkar@adapty.com>
2018-03-23 02:22:43 +0530

    php-info

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
 ?>
