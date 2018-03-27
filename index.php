d972820a8b7c37e590fb3ded3296048caac6d376
Gaurav-adapty <gaaurav.kotkar@adapty.com>
2018-03-27 19:05:40 +0530

    aaa

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-echo "<h2>Build-1</h2>";
+echo "<h2>Hello,</h2>";
 
-echo "<h2>Build-22</h2>";
+echo "This is test page for Jenkins setup<br>";
 
-echo "<h2>Build-33</h2>";
+#echo "Hello world!<br>";
+#echo "Hello world!<br>";
+
 ?>
